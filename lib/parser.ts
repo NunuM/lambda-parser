20c7bd4db80259d7af75495ee2bad3feb5684c61
nuno <1140358@isep.ipp.pt>
2023-07-22 19:15:26 +0100

    wildcard support

--- lib/parser.ts
+++ lib/parser.ts
@@ -1,5 +1,13 @@
 export const FOG_PROTO = 'fog://';
 
+
+const WILD_CARD: string = '*';
+
+const ALL_INSTANCES: number = Number.MAX_VALUE;
+const ALL_VERSIONS: string = WILD_CARD;
+const ALL_LAMBDAS: string = WILD_CARD;
+
+
 /**
  * @class
  */
@@ -7,6 +15,12 @@ export class LambdaURI {
 
     /**
      * @constructor
+     * @param {string} _protocol
+     * @param {string} _regionCountryCode
+     * @param {string} _project
+     * @param {string} _lambdaId
+     * @param {string} _lambdaVersion
+     * @param {number} _lambdaInstance
      */
     constructor(
         private readonly _protocol: string,
@@ -21,7 +35,7 @@ export class LambdaURI {
      * Protocol
      * @return {string}
      */
-    get protocol() {
+    get protocol(): string {
         return this._protocol;
     }
 
@@ -30,7 +44,7 @@ export class LambdaURI {
      *
      * @return {string}
      */
-    get regionCountryCode() {
+    get regionCountryCode(): string {
         return this._regionCountryCode;
     }
 
@@ -39,7 +53,7 @@ export class LambdaURI {
      *
      * @return {string}
      */
-    get project() {
+    get project(): string {
         return this._project;
     }
 
@@ -48,7 +62,7 @@ export class LambdaURI {
      *
      * @return {string}
      */
-    get lambdaId() {
+    get lambdaId(): string {
         return this._lambdaId;
     }
 
@@ -57,7 +71,7 @@ export class LambdaURI {
      *
      * @return {string}
      */
-    get lambdaVersion() {
+    get lambdaVersion(): string {
         return this._lambdaVersion;
     }
 
@@ -66,7 +80,7 @@ export class LambdaURI {
      *
      * @return {number}
      */
-    get lambdaInstance() {
+    get lambdaInstance(): number {
         return this._lambdaInstance;
     }
 
@@ -80,12 +94,176 @@ export class LambdaURI {
     }
 
     /**
+     * Check if is the same project
+     * @param {LambdaURI} other
+     * @return {boolean}
+     */
+    isSameProject(other: LambdaURI): boolean {
+        return this._project === other._project;
+    }
+
+    /**
+     * Check if is the same lambda
+     * @param {LambdaURI} other
+     * @return {boolean}
+     */
+    isSameLambda(other: LambdaURI): boolean {
+        return this.isSameProject(other) && this._lambdaId === other._lambdaId;
+    }
+
+    /**
+     * Check if is the same lambda version
+     * @param {LambdaURI} other
+     * @return {boolean}
+     */
+    isSameLambdaVersion(other: LambdaURI): boolean {
+        return this.isSameLambda(other) && this._lambdaVersion === other._lambdaVersion;
+    }
+
+    /**
+     * Check if is the same lambda instance
+     * @param {LambdaURI} other
+     * @return {boolean}
+     */
+    isSameLambdaInstance(other: LambdaURI): boolean {
+        return this.isSameLambdaVersion(other) && this._lambdaInstance === other._lambdaInstance;
+    }
+
+    /**
+     * Verify if this URI's refers to lambda IDs wild card
+     * @return {boolean}
+     */
+    isToAllLambdas(): boolean {
+        return this.lambdaId === ALL_LAMBDAS;
+    }
+
+    /**
+     * Verify if this URI's refers to lambda version wild card
+     * @return {boolean}
+     */
+    isToAllLambdaVersions(): boolean {
+        return this.lambdaVersion === ALL_VERSIONS;
+    }
+
+    /**
+     * Verify if this URI's refers to lambda instance wild card
+     * @return {boolean}
+     */
+    isToAllLambdaInstances(): boolean {
+        return this.lambdaInstance === ALL_INSTANCES;
+    }
+
+    /**
      * String URI representation
      *
      * @return {string}
      */
     toString() {
-        return `${this.protocol}${this.project}@${this.regionCountryCode}.${this.lambdaId}.${this.lambdaVersion}.${this.lambdaInstance}`;
+        return `${this.protocol}${this.project}@${this.regionCountryCode}.${this.lambdaId}.${this.lambdaVersion}.${this.isToAllLambdaInstances() ? '*' : this.lambdaInstance}`;
+    }
+
+    /**
+     * Create a new URI matching all lambdas
+     * @return {LambdaURI}
+     */
+    toAllLambdas(): LambdaURI {
+        return LambdaURI.allLambdas(this.regionCountryCode, this.project);
+    }
+
+    /**
+     * Create a new URI matching all lambda versions
+     * @return {LambdaURI}
+     */
+    toAllLambdaVersions(): LambdaURI {
+        return LambdaURI.allLambdaVersions(this.regionCountryCode, this.project, this.lambdaId);
+    }
+
+    /**
+     * Create a new URI matching all lambda instances
+     * @return {LambdaURI}
+     */
+    toAllLambdaInstances(): LambdaURI {
+        return LambdaURI.allLambdaInstances(this.regionCountryCode, this.project, this.lambdaId, this.lambdaVersion);
+    }
+
+    /**
+     * Match other lambdaURI with this
+     * @param {LambdaURI} other
+     * @return {boolean}
+     */
+    match(other: LambdaURI): boolean {
+
+        if (!this.isSameProject(other)) {
+            return false;
+        }
+
+        if (!this.isSameLambda(other)) {
+            return other.isToAllLambdas();
+        }
+
+        if (!this.isSameLambdaVersion(other)) {
+            return other.isToAllLambdaVersions();
+        }
+
+        if (!this.isSameLambdaInstance(other)) {
+            return other.isToAllLambdaInstances();
+        }
+
+        return true;
+    }
+
+    /**
+     * Create a new URI matching all lambdas
+     * @param {string} regionCountryCode
+     * @param {string} project
+     * @return {LambdaURI}
+     */
+    static allLambdas(regionCountryCode: string, project: string): LambdaURI {
+        return new LambdaURI(
+            FOG_PROTO,
+            regionCountryCode,
+            project,
+            ALL_LAMBDAS,
+            ALL_VERSIONS,
+            ALL_INSTANCES
+        );
+    }
+
+    /**
+     * Create a new URI matching all lambda versions
+     * @param {string} regionCountryCode
+     * @param {string} project
+     * @param {string} lambdaId
+     * @return {LambdaURI}
+     */
+    static allLambdaVersions(regionCountryCode: string, project: string, lambdaId: string): LambdaURI {
+        return new LambdaURI(
+            FOG_PROTO,
+            regionCountryCode,
+            project,
+            lambdaId,
+            ALL_VERSIONS,
+            ALL_INSTANCES
+        );
+    }
+
+    /**
+     * Create a new URI matching all lambda instances
+     * @param {string} regionCountryCode
+     * @param {string} project
+     * @param {string} lambdaId
+     * @param {string} lambdaVersion
+     * @return {LambdaURI}
+     */
+    static allLambdaInstances(regionCountryCode: string, project: string, lambdaId: string, lambdaVersion: string): LambdaURI {
+        return new LambdaURI(
+            FOG_PROTO,
+            regionCountryCode,
+            project,
+            lambdaId,
+            lambdaVersion,
+            ALL_INSTANCES
+        );
     }
 
     /**
@@ -101,33 +279,36 @@ export class LambdaURI {
             && uri.length > 0
             && uri.length < 255) {
 
-            const matches = uri.match(/(fog:\/\/)?([a-z][a-z\-]+)@([a-z]{2}_[a-z]{2})\.([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})\.([a-z0-9]+)\.?([0-9]+)?/s);
+            let proto = '';
 
-            if (!matches) {
+            if (uri.startsWith(FOG_PROTO)) {
+                proto = uri.substring(0, FOG_PROTO.length);
+            }
+
+            const idx = uri.indexOf("@", proto.length);
+
+            if (idx < 0) {
                 throw new Error("Invalid lambda URI");
             }
 
-            let ignoreProtocol = false;
+            const project = uri.substring(proto.length, idx);
 
-            if (!matches[1]) {
-                ignoreProtocol = true;
+            const [region, id, version, instance] = uri.substring(idx + 1).split(".");
+
+            if (region && project) {
+
+                return new LambdaURI(
+                    FOG_PROTO,
+                    region,
+                    project,
+                    id || ALL_LAMBDAS,
+                    version || ALL_VERSIONS,
+                    Number(instance || ALL_INSTANCES));
             }
 
-            if (!matches[6]) {
-                matches[6] = "1";
-            }
-
-            if (ignoreProtocol || matches.length === 7) {
-                return new LambdaURI(ignoreProtocol ? FOG_PROTO : matches[1],
-                    matches[3],
-                    matches[2],
-                    matches[4],
-                    matches[5],
-                    Number(matches[6])
-                )
-            }
             throw new Error("Invalid lambda URI");
         }
+
         throw new TypeError("URI is invalid data type/length.");
     }
 
--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -20,6 +20,10 @@ describe("LambdaURI", () => {
         expect(() => LambdaURI.parseFromString("fog://pt_pt.prj.32423.zzz.1")).toThrow(Error);
     });
 
+    test("Test wrong format", () => {
+        expect(() => LambdaURI.parseFromString("fog://prj@")).toThrow(Error);
+    });
+
     test("Test valid URI with proto", () => {
         let id = uuid();
         let instance = LambdaURI.parseFromString("fog://prj@se_pt." + id + '.ttt.2');
@@ -54,9 +58,9 @@ describe("LambdaURI", () => {
         expect(instance.regionCountryCode).toBe('pt_pt');
         expect(instance.project).toBe('prj');
         expect(instance.lambdaVersion).toBe("1");
-        expect(instance.lambdaInstance).toBe(1);
+        expect(instance.lambdaInstance).toBe(Number.MAX_VALUE);
 
-        expect(instance.toString()).toBe('fog://' + uri + '.1');
+        expect(instance.toString()).toBe('fog://' + uri + '.*');
     });
 
     test("Test valid URI from obj", () => {
@@ -76,6 +80,50 @@ describe("LambdaURI", () => {
         expect(instance.lambdaInstance).toBe(1);
 
     });
+
+
+    test('match same uris', () => {
+
+        const lambdaURI = new LambdaURI('fog://', 'se_pt', 'test', '11111', '2', 2);
+
+        expect(lambdaURI.match(lambdaURI)).toBe(true);
+
+    })
+
+
+    test('test wild cards instance for same lambda id', () => {
+        const lambdaURI = new LambdaURI('fog://', 'se_pt', 'test', '11111', '2', 2);
+
+        const allLambdaInstances = LambdaURI.allLambdaInstances('se_pt', 'test', '11111', '2');
+
+        expect(lambdaURI.match(allLambdaInstances)).toBe(true);
+
+        const toAllLambdaVersions = allLambdaInstances.toAllLambdaVersions();
+
+        expect(lambdaURI.match(toAllLambdaVersions)).toBe(true);
+
+        const toAllLambdas = allLambdaInstances.toAllLambdas();
+
+        expect(lambdaURI.match(toAllLambdas)).toBe(true);
+    })
+
+    test('test wild cards instance for different lambda id', () => {
+        const lambdaURI = new LambdaURI('fog://', 'se_pt', 'test', '2222', '2', 2);
+
+        const allLambdaInstances = LambdaURI.allLambdaInstances('se_pt', 'test', '3333', '2');
+
+        expect(lambdaURI.match(allLambdaInstances)).toBe(false);
+
+        const toAllLambdaVersions = allLambdaInstances.toAllLambdaVersions();
+
+        expect(lambdaURI.match(toAllLambdaVersions)).toBe(false);
+
+        const toAllLambdas = allLambdaInstances.toAllLambdas();
+
+        expect(lambdaURI.match(toAllLambdas)).toBe(true);
+    })
+
+
 
 });
 
